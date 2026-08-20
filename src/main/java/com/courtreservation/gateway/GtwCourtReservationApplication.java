4b796249102b50bfcf4727d9681c0da2e831cf80
Eduardo Lima <eduardo.lima@devsu.com>
2026-08-20 16:22:30 -0500

    Init gateway

---
+++ src/main/java/com/courtreservation/gateway/GtwCourtReservationApplication.java
@@ -0,0 +1,13 @@
+package com.courtreservation.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtwCourtReservationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GtwCourtReservationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/courtreservation/gateway/GtwCourtReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courtreservation.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtwCourtReservationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
